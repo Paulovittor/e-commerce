23bb357f30fbace2e89453f80561e8f6f8fefa6a
Paulovittor <victor971.pv@gmail.com>
2023-09-20 14:59:22 -0400

    update e-commerce

---
+++ main.js
@@ -0,0 +1,5 @@
+//criação de uma variável que recebe uma string.
+/*o const antes da variável significa que será uma variável constante
+ou seja, que seu valor jamais será alterado*/
+const nomeProduto = 'Casaco Branco';
+const marca = 'Zara';

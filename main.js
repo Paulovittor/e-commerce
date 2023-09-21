01d2d8a4bdf78d3e11c8f449511a08afa0439c04
Paulovittor <victor971.pv@gmail.com>
2023-09-21 15:15:18 -0400

    update

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 //criação de uma variável que recebe uma string.
 /*o const antes da variável significa que será uma variável constante
 ou seja, que seu valor jamais será alterado*/
-const nomeProduto = 'Casaco Branco';
-const marca = 'Zara';
+// const nomeProduto = 'Casaco Branco';
+
+
+/*criação de um objeto em javascript*/
+const produto1 = {
+  nome: 'Casaco Branco',
+  marca: 'Zara',
+  preco: '69.99',
+  nomeArquivoImagem: 'product-1.jpg'
+}
+
+const cartaoProduto = `<div id="card-produto1">
+          <img src="assets/img/product-1.jpg" alt="produto 1 do e-commerce" style="height: 400px;">
+          <p>Zara</p>
+          <p>Casaco branco</p>
+          <p>R$ 69.99</p>
+          <button>Adicionar ao Carrinho</button>
+        </div>`
+
+
